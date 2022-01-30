cd13d11db06ca77874e9b3d61292f68562e89db0
Carlos Ortiz <cartex10@bu.edu>
2022-01-29 19:15:57 -0500

    init commit

---
+++ m_vars.py
@@ -0,0 +1,11 @@
+global timeNoLuck
+global on_check
+global lowerFreq		#Frequency of lower priority reminders DEFAULT: 0.125
+global higherTime		#Amount of timers before higher priority reminder DEFAULT: 10
+global globalTime		#Global timer length in seconds DEFAULT: 30
+
+timeNoLuck = 0
+on_check = False
+lowerFreq = 0.5
+higherTime = 10
+globalTime = 5
