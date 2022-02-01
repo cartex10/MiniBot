ee52b46a25b565595d17e8694527802e5510f61d
Carlos Ortiz <cartex10@bu.edu>
2022-02-01 14:03:27 -0500

    improve notify function

--- m_vars.py
+++ m_vars.py
@@ -1,12 +1,12 @@
 global timeNoLuck
 global on_check
 global lowerFreq		#Frequency of lower priority reminders DEFAULT: 0.125
-global higherTime		#Amount of timers before higher priority reminder DEFAULT: 10
+global maxTimers		#Amount of timers before higher priority reminder DEFAULT: 10
 global globalTime		#Global timer length in seconds DEFAULT: 30
 global con
 
 timeNoLuck = 0
 on_check = False
-lowerFreq = 0.5
-higherTime = 10
-globalTime = 5
+lowerFreq = 0.125
+maxTimers = 10
+globalTime = 30
