4a2a0f02a42ca7f3e18fefec793bfbe28a4d28fa
Carlos Ortiz <cartex10@bu.edu>
2022-02-01 11:48:55 -0500

    add reminder view

--- m_functions.py
+++ m_functions.py
@@ -1,9 +1,10 @@
 import asyncio, sqlite3
 import nextcord as discord
 from nextcord.ext import commands
-import random
+import random, math
 from m_vars import *
 
+# Classes
 class Timer:
 	def __init__(self, timeout, callback, args=None):
 		self._timeout = timeout
@@ -19,21 +20,51 @@ class Timer:
 		self._task.cancel()
 
 class reminderView(discord.ui.View):
-	def __init__(self):
+	def __init__(self, bot, msg):
 		super().__init__()
-		self.sort = 0
+		self.sort = -1
+		self.bot = bot 
+		self.msg = msg
+		self.selected = 0
+	async def update(self):
+		cursor = await getReminders(self.sort)
+		msgtext = "```CURRENT REMINDERS\t\t\tPTY\t\tSORT: "
+		if self.sort == -1:
+			msgtext += "ALL\n"
+		elif self.sort == 0:
+			msgtext += "LP\n"
+		elif self.sort == 1:
+			msgtext += "HP\n"
+		count = 1
+		for item in cursor:
+			if self.selected == count - 1:
+				msgtext += " >> "
+			msgtext += str(count) + ". " + item[0] # + "\t\t" + str(item[1])
+			if self.selected == count - 1:
+				for i in range(1, math.floor((30 - len(item[0])) / 4)):
+					msgtext += "\t"
+			else:
+				for i in range(0, math.floor((30 - len(item[0])) / 4)):
+					msgtext += "\t"
+			if not item[1]:
+				msgtext += "LP"
+			elif item[1]:
+				msgtext += "HP"
+			if self.selected == count - 1:
+				msgtext += " << "
+			msgtext += "\n"
+			count += 1
+		msgtext += "```"
+		await self.msg.edit(msgtext)
 	@discord.ui.button(label='SORT', style=discord.ButtonStyle.secondary)
 	async def sort(self, button: discord.ui.Button, interaction: discord.Interaction):
 		self.sort += 1
-		if self.sort > 2:
-			self.sort = 0
-		if self.sort is 0:
-				print(self.sort)
-		elif self.sort is 1:
-				print(self.sort)
-		elif self.sort is 2:
-				print(self.sort)
+		if self.sort > 1:
+			self.sort = -1
+		await self.update()
+		
 
+# Timers
 async def start_timer(args):
 	chan = args["chan"]
 	await timesUp(chan)
@@ -49,12 +80,27 @@ async def timesUp(chan):
 
 	timer = Timer(globalTime, start_timer, args={'chan':chan})
 
+# Database Functions
 async def checkConnection(chan):
+	global con
 	await chan.send("```Attempting database connection```")
 	con = sqlite3.connect("m_db.db")
 	try:
-		cursor = con.execute("SELECT userID, guild, title, priority FROM REMINDERS")
+		cursor = con.execute("SELECT title, priority FROM REMINDERS")
 		await chan.send("```Connection successful!```")
 	except:
 		await chan.send("```Creating REMINDERS table```")
-		cursor = con.execute("CREATE TABLE REMINDERS (userID INT PRIMARY KEY NOT NULL, guild INT NOT NULL, title TEXT NOT NULL, priority BOOL NOT NULL);")
+		cursor = con.execute("CREATE TABLE REMINDERS (title TEXT PRIMARY KEY NOT NULL, priority BOOL NOT NULL);")
+
+async def getReminders(priority):
+	global con
+	if priority < 0:
+		cursor = con.execute("SELECT title, priority FROM REMINDERS")
+	else:
+		cursor = con.execute("SELECT title, priority FROM REMINDERS WHERE priority=?", (bool(priority),))
+	return cursor
+
+async def addReminder(title, priority):
+	global con
+	con.execute("INSERT INTO REMINDERS VALUES (?, ?) ", (title, bool(priority)))
+	con.commit()
