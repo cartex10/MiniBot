7c95fd4453b94c44cfcf32dbec34c6882ca06ab1
Carlos Ortiz <cartex10@bu.edu>
2022-02-13 03:42:54 -0500

    change default values

--- m_vars.py
+++ m_vars.py
@@ -1,10 +1,10 @@
 global timeNoLuck
 global on_check
-global lowerFreq			# Frequency of lower priority reminders 							DEFAULT: 0.125
-global personalityOverride	# Chance of notifications to be overriden by personality message	DEFAULT: 0.33
-global maxTimers			# Amount of timers before higher priority reminder 					DEFAULT: 10
-global notifyTime			# Global timer length in seconds 									DEFAULT: 153
-global mangaTime			# Time between manga checks 										DEFAULT: 300
+global lowerFreq			# Frequency of lower priority reminders 							DEFAULT: lowerFreq = 0.125
+global personalityOverride	# Chance of notifications to be overriden by personality message	DEFAULT: personalityOverride = 0.20
+global maxTimers			# Amount of timers before higher priority reminder 					DEFAULT: maxTimers = 15
+global notifyTime			# Global timer length in seconds 									DEFAULT: notifyTime = 603
+global mangaTime			# Time between manga checks 										DEFAULT: mangaTime = 300
 global con
 global n_timer
 global m_timer
@@ -12,7 +12,7 @@ global m_timer
 timeNoLuck = 0
 on_check = False
 lowerFreq = 0.125
-personalityOverride = 0.33
-maxTimers = 10
-notifyTime = 153
+personalityOverride = 0.20
+maxTimers = 15
+notifyTime = 603
 mangaTime = 300
